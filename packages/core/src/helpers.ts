ed7003befc25dc2013eee7e66fd63b935052fb89
Коротенко Артур <akorotenko@rnds.pro>
2021-01-15 12:10:23 +0300

    Fix resolve ext

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -1,33 +1,34 @@
-import fs from 'fs';
-import path from 'path';
-import webpack from 'webpack';
-import readdir from 'recursive-readdir';
+import fs from 'fs'
+import path from 'path'
+import webpack from 'webpack'
+import readdir from 'recursive-readdir'
 
-const cwd: string = process.cwd();
+const cwd: string = process.cwd()
 
 export const existsSync = (f: string): boolean => {
   try {
-    fs.accessSync(f, fs.constants.F_OK);
-    return true;
+    fs.accessSync(f, fs.constants.F_OK)
+    return true
   } catch (_) {
-    return false;
+    return false
   }
-};
+}
 
 export const isProd = () => {
-  let env = process.env.NODE_ENV || 'development';
+  let env = process.env.NODE_ENV || 'development'
   if (process.env.REACT_SSR_ENV) {
-    env = process.env.REACT_SSR_ENV;
+    env = process.env.REACT_SSR_ENV
   }
-  return env === 'production';
-};
+  return env === 'production'
+}
 
 export interface Config {
-  id: string;
-  distDir: string;
-  viewsDir: string;
-  staticViews: string[];
-  webpack?: (defaultConfig: webpack.Configuration, env: 'development' | 'production') => webpack.Configuration;
+  id: string
+  distDir: string
+  viewsDir: string
+  staticViews: string[]
+  ext: 'jsx' | 'tsx'
+  webpack?: (defaultConfig: webpack.Configuration, env: 'development' | 'production') => webpack.Configuration
 }
 
 const getSsrConfig = (): Config => {
@@ -36,41 +37,42 @@ const getSsrConfig = (): Config => {
     distDir: '.ssr',
     viewsDir: 'views',
     staticViews: [],
-  };
-  const ssrConfigPath = path.join(cwd, 'ssr.config.js');
-  if (existsSync(ssrConfigPath)) {
-    return Object.assign(defaultConfig, require(ssrConfigPath));
-  } else {
-    return defaultConfig;
+    ext: 'jsx'
   }
-};
+  const ssrConfigPath = path.join(cwd, 'ssr.config.js')
+  if (existsSync(ssrConfigPath)) {
+    return Object.assign(defaultConfig, require(ssrConfigPath))
+  } else {
+    return defaultConfig
+  }
+}
 
-export const getEngine = (): 'jsx' | 'tsx' => existsSync(path.join(cwd, 'tsconfig.json')) ? 'tsx' : 'jsx';
+export const getEngine = (): 'jsx' | 'tsx' => ssrConfig.ext
 
-export const ssrConfig: Config = getSsrConfig();
+export const ssrConfig: Config = getSsrConfig()
 
 export const getPages = async (): Promise<string[]> => {
-  const possibles = await readdir(path.join(cwd, ssrConfig.viewsDir));
-  const pages = [];
+  const possibles = await readdir(path.join(cwd, ssrConfig.viewsDir))
+  const pages = []
   for (let i = 0; i < possibles.length; i++) {
-    const possible = possibles[i];
-    const name = path.basename(getPageId(possible, '/'));
+    const possible = possibles[i]
+    const name = path.basename(getPageId(possible, '/'))
     if (name.toLowerCase().startsWith('_app') || name.toLowerCase().startsWith('_document')) {
-      continue;
+      continue
     }
     if (possible.endsWith('.jsx') || possible.endsWith('.tsx')) {
-      pages.push(possible);
+      pages.push(possible)
     }
   }
-  return pages;
-};
+  return pages
+}
 
 export const getPageId = (page: string, separator: string = '_'): string => {
   const [, ...rest] = page.replace(path.join(cwd, ssrConfig.viewsDir), '')
-                          .replace(path.extname(page), '')
-                          .split(path.sep);
-  return rest.join(separator);
-};
+    .replace(path.extname(page), '')
+    .split(path.sep)
+  return rest.join(separator)
+}
 
 const ignores = [
   '.*',
@@ -80,16 +82,16 @@ const ignores = [
   '*.txt',
   '*.yml',
   'LICENSE',
-];
+]
 
 const ignoreDotDir = (file: string, stats: fs.Stats) => {
-  return stats.isDirectory() && path.basename(file).startsWith('.');
-};
+  return stats.isDirectory() && path.basename(file).startsWith('.')
+}
 
 const ignoreNodeModules = (file: string, stats: fs.Stats) => {
-  return stats.isDirectory() && path.basename(file) == 'node_modules';
-};
+  return stats.isDirectory() && path.basename(file) == 'node_modules'
+}
 
 export const getCacheablePages = async (): Promise<string[]> => {
-  return readdir(cwd, [ignoreNodeModules, ignoreDotDir, ...ignores]);
-};
+  return readdir(cwd, [ignoreNodeModules, ignoreDotDir, ...ignores])
+}
